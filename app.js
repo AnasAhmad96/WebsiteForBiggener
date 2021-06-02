4a0f4fcb6765532266d797beb9fe50264e000301
AnasAhmed13 <>
2021-06-02 13:53:41 +0000

    new project

---
+++ app.js
@@ -0,0 +1,10 @@
+alert('welcome to WebsiteForBiggener')
+
+var answer = prompt('Are you biggener ?')
+
+if(answer === 'yes'){
+  alert('this course for you , Welcome')
+}
+else{
+  alert('you are not allowed to enter the website')
+}
